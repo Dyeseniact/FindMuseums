41c460a8245b5486c669877529bed6bb119bbcb9
damianSM1997 <damiansm97@gmail.com>
2021-02-23 23:07:54 -0600

    se agrega clase base para el proyecto

---
+++ index.js
@@ -0,0 +1,10 @@
+class Mascota{
+	constructor(nombre, categoria, fotos, descripcion, anunciante, ubicacion){
+		this.nombre = nombre;
+		this.categoria = categoria;
+		this.fotos = fotos; 
+		this.descripcion = descripcion;
+		this.anunciante = anunciante; 
+		this.ubicacion = ubicacion; 
+	}	
+}
