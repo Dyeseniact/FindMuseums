f0be53f6d675135e2dc9c2059cc2077aab627698
Anahi <anahicamash@gmail.com>
2021-02-28 17:45:10 -0600

    Service module

---
+++ models/Service.js
@@ -0,0 +1,13 @@
+class Service{
+	constructor(idService, description,date, time, price, limitVisitors, ) {
+		this.idService = idService;
+		this.description = description;
+		this.date = date;
+		this.time = time;
+		this.price = price;
+		this.limitVisitors = limitVisitors;
+		
+	}
+}
+
+module.exports = Service;
