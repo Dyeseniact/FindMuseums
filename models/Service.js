8a3d806e01ff90cafe10043cee9819d104a2b162
Anahi <anahicamash@gmail.com>
2021-02-28 18:40:52 -0600

    services route and controller

--- models/Service.js
+++ models/Service.js
@@ -1,11 +1,12 @@
 class Service{
-	constructor(idService, description,date, time, price, limitVisitors, ) {
+	constructor(idService, description,date, time, price, limitVisitors, status ) {
 		this.idService = idService;
 		this.description = description;
 		this.date = date;
 		this.time = time;
 		this.price = price;
 		this.limitVisitors = limitVisitors;
+        this.status = status;
 		
 	}
 }
--- routes/index.js
+++ routes/index.js
@@ -10,6 +10,7 @@ router.get('/', (req, res)=>{
 
 router.use('/usuarios', require('./usuarios'));
 router.use('/mascotas', require('./mascotas'));
+router.use('/services', require('./services'));
 
 /* con el método use de nuestro router estamos indicando 
 * que en la ruta 'v1/usuarios' estarán anidadas las rutas 
